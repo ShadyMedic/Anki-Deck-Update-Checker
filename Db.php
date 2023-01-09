82f6164a7e0f9535483c628200c207738659d003
Jan Štěch <jan.stech@posteo.net>
2023-01-09 10:42:15 +0100

    Initial commit

---
+++ Db.php
@@ -0,0 +1,25 @@
+<?php
+
+
+class Db
+{
+    private const DB_HOST = 'localhost';
+    private const DB_USER = 'root';
+    private const DB_PASS = ' ';
+    private const DB_NAME = 'ankipacks';
+    private const DB_SETTINGS = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_EMULATE_PREPARES => false
+    );
+    
+    /**
+     * Connects to the database
+     * @return PDO Database connection
+     */
+    public static function connect(): PDO
+    {
+        $pdo = new PDO('mysql:host='.self::DB_HOST.';dbname='.self::DB_NAME, self::DB_USER, self::DB_PASS, self::DB_SETTINGS);
+        return $pdo;
+    }
+}
---
+++ check-update.php
@@ -0,0 +1,33 @@
+<?php
+
+$packageId = $_GET['id']; //At least 1
+$currentVersion = $_GET['current']; //First version is 1
+
+if (empty($packageId) || empty($currentVersion)) {
+    header("HTTP/1.0 400 Bad Request");
+    die();
+}
+
+require 'Db.php';
+$db = Db::connect();
+
+$statement = $db->prepare('SELECT version AS "v",download_link AS "l" FROM package WHERE package_id = ? LIMIT 1;');
+$statement->execute(array($packageId));
+$packageData = $statement->fetch();
+
+if (empty($packageData)) {
+    readfile('not-found.png');
+    die();
+}
+
+header ('Content-Type: image/png');
+
+if ($currentVersion >= $packageData['v']) {
+    readfile('up-to-date.png');
+    die();
+} else {
+    readfile('outdated.png');
+    $downloadLink = $packageData['l'];
+    //TODO display the link somewhere
+    die();
+}
