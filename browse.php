0c75cb806d18b0cca2208fb8037b69136aea72d6
Jan Štěch <jan.stech@posteo.net>
2023-02-26 20:31:53 +0100

    Add author field to the database and to the browse table

--- browse.php
+++ browse.php
@@ -1,7 +1,7 @@
 <?php
 
 $accessKey = null;
-$query = 'SELECT package_id,filename,version,updated_at FROM package WHERE access_key IS NULL;';
+$query = 'SELECT package_id,filename,author,version,updated_at FROM package WHERE access_key IS NULL AND version > 0 AND download_link IS NOT NULL ORDER BY updated_at DESC;';
 
 require 'Db.php';
 $db = Db::connect();
@@ -21,30 +21,41 @@ $packages = $statement->fetchAll();
             text-align: center;
             padding: 0 16px;
         }
+
         .txt-l {
             text-align: left;
         }
     </style>
 </head>
 <body>
-    <header>
-        <h1>Public Anki Decks</h1>
-    </header>
-    <article>
-        <p>This is the list of all public Anki decks uploaded to this webserver.</p>
-        <p>All decks uploaded are considered public, if they don't have an access key specified.</p>
-        <p>To view more information about any file or to download it, simply click it's name.</p>
-        
-        <table>
-            <tr><th class="txt-l">Deck name</th><th>Version number</th><th>Last updated at</th></tr>
-            <?php foreach ($packages as $package) : ?>
-                <tr>
-                    <td class="txt-l"><a href="/deck.php?id=<?= $package['package_id'] ?>"><?= $package['filename'] ?></a></td>
-                    <td><?= $package['version'] ?></td>
-                    <td><?= $package['updated_at'] ?></td>
-                </tr>
-            <?php endforeach ?>
-        </table>
-    </article>
+<header>
+    <h1>Public Anki Decks</h1>
+</header>
+<article>
+    <p>This is the list of all public Anki decks uploaded to this webserver.</p>
+    <p>All decks uploaded are considered public, if they don't have an access key specified.</p>
+    <p>To view more information about any file or to download it, simply click it's name.</p>
+
+    <table>
+        <tr>
+            <th class="txt-l">Deck name</th>
+            <th class="txt-l">Author</th>
+            <th>Version number</th>
+            <th>Last updated at</th>
+        </tr>
+        <?php foreach ($packages as $package) : ?>
+            <tr>
+                <td class="txt-l">
+                    <a href="/deck.php?id=<?= $package['package_id'] ?>">
+                        <?= $package['filename'] ?>
+                    </a>
+                </td>
+                <td class="txt-l"><?= $package['author'] ?></td>
+                <td><?= $package['version'] ?></td>
+                <td><?= $package['updated_at'] ?></td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</article>
 </body>
 </html>
