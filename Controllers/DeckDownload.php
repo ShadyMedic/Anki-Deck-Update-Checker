07e7da2d783684af2a96ba0b8629251e69aa09d5
ShadyMedic <19894267+ShadyMedic@users.noreply.github.com>
2026-05-12 17:27:34 +0000

    Překlad

--- Controllers/DeckDownload.php
+++ Controllers/DeckDownload.php
@@ -21,24 +21,24 @@ class DeckDownload extends Controller
         $packageFound = $package->load($packageId);
 
         if (!$packageFound) {
-            throw new UserException('No package with this ID was found.', 404002);
+            throw new UserException('Balíček s tímto ID nebyl nalezen.', 404002);
         }
 
         if ($package->isDeleted()) {
-            throw new UserException('This package was deleted.', 410002);
+            throw new UserException('Tento balíček byl smazán.', 410002);
         }
 
         $authenticator = new PackageManager();
         if (!$authenticator->checkReadAccess($packageId, $accessKey)) {
-            throw new UserException('This package is private and the access key is either wrong or missing.', 401002);
+            throw new UserException('Tento balíček je soukromý a přístupový klíč buďto chybí nebo je nesprávný.', 401002);
         }
 
         if ($package->getVersion() === 0) {
-            throw new UserException('This package hasn\'t been uploaded yet.', 406002);
+            throw new UserException('Tento balíček zatím nebyl nahrán.', 406002);
         }
 
         if (!file_exists('decks/'.$packageId.'.apkg')) {
-            throw new UserException('This package file doesn\'t exist on our servers.', 404003);
+            throw new UserException('Soubor s tímto balíčkem na našem serveru neexistuje.', 404003);
         }
 
         self::$views = []; //Don't output any HTML
